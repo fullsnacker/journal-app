bd1ffc2a9d49ce8cab69c259bdb72a3b761c8d2a
fullsnacker <fullsnacker@gmail.com>
2024-03-11 13:00:16 -0300

    Added firebase config - working

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,7 +1,15 @@
+import { useDispatch } from 'react-redux';
 import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material';
 import { LogoutOutlined, MenuOutlined } from '@mui/icons-material';
+import { startLogout } from '../../store/auth';
 
 export const NavBar = ({ drawerWidth = 240 }) => {
+	const dispatch = useDispatch();
+
+	const onLogout = () => {
+		dispatch(startLogout());
+	};
+
 	return (
 		<AppBar
 			position="fixed"
@@ -30,7 +38,7 @@ export const NavBar = ({ drawerWidth = 240 }) => {
 						JournalApp{' '}
 					</Typography>
 
-					<IconButton color="error">
+					<IconButton color="error" onClick={onLogout}>
 						<LogoutOutlined />
 					</IconButton>
 				</Grid>
